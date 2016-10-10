3a7a2c6d7f822b6f2e8d3af356cf8341f394be0c
Dileep92 <dileepmenon92.@yahoo.com>
2016-10-10 19:16:03 +0530

    Change function input_parameters to geom_create

--- 01_pre_processor/pre_processor.py
+++ 01_pre_processor/pre_processor.py
@@ -3,15 +3,14 @@ import numpy as np
 tan = np.tan
 
 
-
-
-def input_parameters():
-    #r - root, t - tip, b - span, le - leading edge, te - trailing edge, AR - aspect ratio 
+def geom_create():
+    #Wing parameters where r - root, t - tip, b - span, le - leading edge, te - trailing edge, S - planform area, AR - aspect ratio 
     r_chord = 1
     taper_ratio = 2
     t_chord = r_chord/taper_ratio
     b = 10
     sweep_angle = 20 
+    #Wing Co-ordinates
     r_le = (0.0, 0.0)
     r_te = (r_chord, 0.0)
     t_le = (0.5*b*tan(math.radians(sweep_angle)), 0.5*b) 
