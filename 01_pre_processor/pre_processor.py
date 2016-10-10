0cc6299cc04ec5dc6687f78a9f6f314b5011408a
Dileep92 <dileepmenon92.@yahoo.com>
2016-10-10 15:44:37 +0530

    Create module for geometry creation

---
+++ 01_pre_processor/pre_processor.py
@@ -0,0 +1,21 @@
+import math
+import numpy as np
+tan = np.tan
+
+
+
+
+def input_parameters():
+    #r - root, t - tip, b - span, le - leading edge, te - trailing edge, AR - aspect ratio 
+    r_chord = 1
+    taper_ratio = 2
+    t_chord = r_chord/taper_ratio
+    b = 10
+    sweep_angle = 20 
+    r_le = (0.0, 0.0)
+    r_te = (r_chord, 0.0)
+    t_le = (0.5*b*tan(math.radians(sweep_angle)), 0.5*b) 
+    t_te = (0.5*b*tan(math.radians(sweep_angle)) + t_chord, 0.5*b)
+    S = 0.5*b*t_le[0] + b*(t_te[0]-t_le[0]) + 0.5*b*(r_chord-t_te[0])
+    AR = b**2/S
+    return r_le, r_te, t_le, t_te
