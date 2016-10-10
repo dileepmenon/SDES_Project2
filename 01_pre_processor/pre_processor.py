d16484c5b8df58acab694fe7e70ae035c94ed620
Dileep92 <dileepmenon92.@yahoo.com>
2016-10-11 00:11:50 +0530

    Create function to mesh geometry

--- 01_pre_processor/pre_processor.py
+++ 01_pre_processor/pre_processor.py
@@ -17,4 +17,20 @@ def geom_create():
     t_te = (0.5*b*tan(math.radians(sweep_angle)) + t_chord, 0.5*b)
     S = 0.5*b*t_le[0] + b*(t_te[0]-t_le[0]) + 0.5*b*(r_chord-t_te[0])
     AR = b**2/S
-    return r_le, r_te, t_le, t_te
+    return r_le, r_te, t_le, t_te, sweep_angle
+
+def mesh_create(r_le, r_te, t_le, t_te, sweep_angle):
+    le_ygrid = np.linspace(r_le[1], t_le[1], 20)
+    le_xgrid = list(map(lambda y : y*tan(math.radians(sweep_angle)), le_ygrid))
+    le_xy_grid = [(x,y) for x,y in zip(le_xgrid, le_ygrid)]
+    te_ygrid = le_ygrid[:]
+    te_xgrid = list(map(lambda y : r_te[0]-(y*((r_te[0]-t_te[0])/t_te[1])), te_ygrid))
+    te_xy_grid = [(x,y) for x,y in zip(te_xgrid, te_ygrid)]
+    chord_endpoints = [[pos_le, pos_te] for pos_le,pos_te in zip(le_xy_grid, te_xy_grid)]
+    span_grid = map(lambda (pos_le, pos_te) : [(x,y) for x,y in zip(np.linspace(pos_le[0], pos_te[0], 5), np.linspace(pos_le[1], pos_te[1], 5))], chord_endpoints)   
+    return span_grid
+
+
+    
+
+
